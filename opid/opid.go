70384fff296bb920e6764b1d4b41a2de642f80ec
Elad Tabak <etabak@redhat.com>
2025-03-10 12:16:30 +0200

    Expose opid key

--- opid/opid.go
+++ opid/opid.go
@@ -10,14 +10,14 @@ import (
 type ContextKey int
 
 const (
-	// opidKey is the key used to store operation identifiers in context
-	opidKey ContextKey = iota
+	// OpidKey is the key used to store operation identifiers in context
+	OpidKey ContextKey = iota
 )
 
 // FromContext returns the operation identifier from the given context, or an empty string if no
 // operation identifier is attached to the context.
 func FromContext(ctx context.Context) string {
-	return fromContext(ctx, opidKey)
+	return fromContext(ctx, OpidKey)
 }
 
 func fromContext(ctx context.Context, key ContextKey) string {
@@ -30,16 +30,16 @@ func fromContext(ctx context.Context, key ContextKey) string {
 
 // IntoContext creates a new context containing the given operation identifier.
 func IntoContext(ctx context.Context, id string) context.Context {
-	return context.WithValue(ctx, opidKey, id)
+	return context.WithValue(ctx, OpidKey, id)
 }
 
 // WithOpId creates a new context on top of the new one that contains a new operation identifier.
 func WithOpId(ctx context.Context) context.Context {
-	if ctx.Value(opidKey) != nil {
+	if ctx.Value(OpidKey) != nil {
 		return ctx
 	}
 	opId := uuid.NewString()
-	return context.WithValue(ctx, opidKey, opId)
+	return context.WithValue(ctx, OpidKey, opId)
 }
 
 func NewContext() context.Context {
@@ -48,6 +48,6 @@ func NewContext() context.Context {
 }
 
 func CopyOpId(src context.Context, dst context.Context) context.Context {
-	opID := src.Value(opidKey)
-	return context.WithValue(dst, opidKey, opID)
+	opID := src.Value(OpidKey)
+	return context.WithValue(dst, OpidKey, opID)
 }
